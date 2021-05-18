f5a0f8fe76772c3e046ff02804bc067e18a6f2ba
maximilianozurita <maxi.zurita.seb@gmail.com>
2021-05-18 20:56:38 -0300

    Login/register

--- Maxi/app.js
+++ Maxi/app.js
@@ -2,17 +2,22 @@ const express=require("express");
 const app=express();
 const path=require("path");
 
-app.listen(3000,()=>{
+app.listen(3030,()=>{
     console.log("Servidor ejecutado");
 });
 
 
 //Lo que esta en la carpeta public sera usado.
-const publicPath=path.resolve(__dirname, "./public");
+const publicPath=path.resolve(__dirname + "/public");
 
 
 //Utilizar la carpeta y todas sus cosas de manera estatica (establece como raiz de todos los directorios)
 app.use(express.static(publicPath));
 
+//-----------------------------
+app.get ("/register",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/register.html"))})
+app.get ("/login",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/login.html"))})
 //permite dar una ruta absoluta
 app.get ("/",(req,res)=>{res.sendFile(path.resolve(__dirname, "./views/home.html"))})
+
+
