6338d218e3acc8b6134bc4de7b92b93869bbb888
jeremias256 <gab.menacho@gmail.com>
2021-05-25 11:31:00 -0300

    agrege archivos de proyecto individual

--- Jeremias/app.js
+++ Jeremias/app.js
@@ -1,16 +1,24 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const port = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, "./public");
 app.use( express.static(publicPath));
 
-
-app.listen(3002,() =>
+app.listen(port, function()
 {
-    console.log("Mercado liebre corriendo");
+    console.log("servidor corriendo en puerto 3000");
 });
 
 app.get("/",(req,res) =>
 {
-    res.sendFile(path.resolve(__dirname, "./views/home.html"));
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
 }); 
+app.get("/register",(req,res) =>
+{
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
+}); 
+app.get("/login",(req,res) =>
+{
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
+});
